283978645894e9858ae984d64105a1492d7396c3
Michael Vogt <mvo@ubuntu.com>
2021-08-01 10:42:58 +0200

    urequests: add timeout parameter

--- python-ecosys/urequests/urequests.py
+++ python-ecosys/urequests/urequests.py
@@ -33,7 +33,7 @@ class Response:
         return ujson.loads(self.content)
 
 
-def request(method, url, data=None, json=None, headers={}, stream=None):
+def request(method, url, data=None, json=None, headers={}, stream=None, timeout=None):
     try:
         proto, dummy, host, path = url.split("/", 3)
     except ValueError:
@@ -56,6 +56,8 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
     ai = ai[0]
 
     s = usocket.socket(ai[0], ai[1], ai[2])
+    if timeout:
+        s.settimeout(timeout)
     try:
         s.connect(ai[-1])
         if proto == "https:":
